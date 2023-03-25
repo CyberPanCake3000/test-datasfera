7498392d169d77636ec6439dc896b331d083eb6b
egatapova <ergatapovaer@gmail.com>
2023-03-25 23:19:10 +0300

    init

---
+++ app/Models/ApiConnection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use AmoCRM\OAuth2\Client\Provider\AmoCRM;
+
+class ApiConnection extends Model
+{
+    use HasFactory;
+}
